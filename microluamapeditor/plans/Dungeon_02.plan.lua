8b8179db343f52a85f63204cda7560b648e5db0c
ghuntar.waldmeister <ghuntar.waldmeister@fce2f63a-a208-11de-ba4d-5dbaeb624ccf>
2009-10-27 23:09:31 +0000

    reorganisation of files in mapeditor

---
+++ microluamapeditor/plans/Dungeon_02.plan.lua
@@ -0,0 +1,45 @@
+-- Dungeon_01.plan
+
+-- Taille des tuiles
+tile_width  = 16
+tile_height = 16
+
+-- Nombre de tuiles sur la carte
+map_width = 50
+map_height = 50
+
+-- Fichier Map
+smap = {}
+smap.BG_map = "./maps/Dungeon_02_BG.map"
+smap.FG_map = "./maps/Dungeon_02_FG.map"
+
+-- Fichier Tileset
+smap.BG_Tileset = Image.load("./images/Ghuntlet_dungeon.png", VRAM)
+-- smap.FG_tileset = Image.load("./images/Ghuntlet_dungeon.png", VRAM) -- On utilise le même fichier
+
+-- Init (Scroll)Map
+smap.BG_smap = ScrollMap.new(smap.BG_Tileset, smap.BG_map, map_width, map_height, tile_width, tile_height)
+smap.FG_smap = ScrollMap.new(smap.BG_Tileset, smap.FG_map, map_width, map_height, tile_width, tile_height)
+smap.BG_blocking_tiles = {129,139,54,55,56,57,63,80,81,82,85,0,1,2,5,16,32,21,37,143,78,94,95,110,126}
+smap.doors = {143}
+smap.stairs = {174}
+smap.default_tile = 17
+
+game.level = 2
+hero_startpos = {120 , 120}
+maxrealx = map_width * tile_width
+maxrealy = map_height * tile_height
+
+smap.monster_list = {
+					{"Portal",{200 , 200}},
+--[[					{"Ratmut",{200 , 200}},
+					{"BlackMage",{220 , 280}},
+					{"Skeleton",{250,250}},
+					{"Ratmut",{210 , 280}},
+					{"BlackMage",{210 , 310}},
+					{"Skeleton",{240,240}},
+					{"Scarab",{270,140}},
+]]					}
+smap.item_list =	{
+					{"key",{150,140}},
+					}
---
+++ microluamapeditor/plans/map_num.plan.lua
@@ -0,0 +1,32 @@
+-- map_num.plan.lua
+
+-- taille des tuiles
+tile_width  = 16
+tile_height = 16
+
+-- nombre de tuiles sur la carte
+map_width = 6
+map_height = 6
+
+-- fichier map
+smap = {}
+smap.BG_map = "./maps/map_num_BG.map"
+smap.FG_map = "./maps/map_num_FG.map"
+
+-- Fichier Tileset
+smap.BG_Tileset = Image.load("./images/map_num.png", VRAM)
+smap.FG_tileset = Image.load("./images/Ghuntlet_dungeon.png", VRAM)
+
+-- Init (Scroll)Map
+smap.BG_smap = ScrollMap.new(smap.BG_Tileset, smap.BG_map, map_width, map_height, tile_width, tile_height)
+smap.FG_smap = ScrollMap.new(smap.BG_Tileset, smap.FG_map, map_width, map_height, tile_width, tile_height)
+smap.BG_blocking_tiles = {16, 129 , 139}
+smap.doors = {143}
+
+hero_startpos = {16 , 16}
+maxrealx = map_width * tile_width
+maxrealy = map_height * tile_height
+smap.monster_list = {}
+smap.item_list =	{
+					{"key",{16,40}},
+					}
