28126f0dcd05049f74bea3011f0731fc3676da75
ghuntar.waldmeister <ghuntar.waldmeister@fce2f63a-a208-11de-ba4d-5dbaeb624ccf>
2010-04-30 21:03:05 +0000

    function COORD.__eq(obj1,obj2), function COORD:toMAPCOORD() added. Event (stairs and doors) implemented. function gameover () slightly modified. Movement computing slightly modified (var MOB.inmove added). function MOB:collide_tile(other) added to see if it can improve FPS. Base_Camp and Gameover map/plan added. Other maps/plans updated (to comply with event handling).

--- trunk/Ghuntlet/plans/Dungeon_01.plan.lua
+++ trunk/Ghuntlet/plans/Dungeon_01.plan.lua
@@ -22,12 +22,10 @@ smap.BG_Tileset = Image.load("./images/Ghuntlet_dungeon.png", VRAM)
 smap.BG_smap = ScrollMap.new(smap.BG_Tileset, smap.BG_map, smap.map_width, smap.map_height, smap.tile_width, smap.tile_height)
 smap.FG_smap = ScrollMap.new(smap.BG_Tileset, smap.FG_map, smap.map_width, smap.map_height, smap.tile_width, smap.tile_height)
 smap.BG_blocking_tiles = {129,139,54,55,56,57,63,80,81,82,85,0,1,2,5,16,32,21,37,143,78,94,95,110,126}
-smap.doors = {143}
-smap.stairs = {174}
 smap.default_tile = 132
 smap.maxrealx = smap.map_width * smap.tile_width
 smap.maxrealy = smap.map_height * smap.tile_height
-smap.level = 1
+-- smap.level = 1
 smap.hero_startpos = COORD:new({x = 120 , y = 120})
 smap.scroll = COORD:new()
 smap.offset = COORD:new({x=-8,y=-8})
@@ -45,3 +43,9 @@ smap.item_list =	{
 					{"doubleaxe",{250,200}},
 					{"doubleaxe",{200,250}},
 					}
+                    
+smap.event_list =   {
+                    {coordm = COORD:new({x=16,y=14}),event_type = "door"},
+                    {coordm = COORD:new({x=17,y=14}),event_type = "stairs", level = "./plans/Dungeon_02.plan.lua"},
+                    {coordm = COORD:new({x=18,y=14}),event_type = "door"}
+                    }
--- trunk/Ghuntlet/plans/Dungeon_02.plan.lua
+++ trunk/Ghuntlet/plans/Dungeon_02.plan.lua
@@ -1,15 +1,16 @@
--- Dungeon_01.plan
+-- Dungeon_02.plan
+
+smap = {}
 
 -- Taille des tuiles
-tile_width  = 16
-tile_height = 16
+smap.tile_width  = 16
+smap.tile_height = 16
 
 -- Nombre de tuiles sur la carte
-map_width = 50
-map_height = 50
+smap.map_width = 50
+smap.map_height = 50
 
 -- Fichier Map
-smap = {}
 smap.BG_map = "./maps/Dungeon_02_BG.map"
 smap.FG_map = "./maps/Dungeon_02_FG.map"
 
@@ -18,17 +19,18 @@ smap.BG_Tileset = Image.load("./images/Ghuntlet_dungeon.png", VRAM)
 -- smap.FG_tileset = Image.load("./images/Ghuntlet_dungeon.png", VRAM) -- On utilise le même fichier
 
 -- Init (Scroll)Map
-smap.BG_smap = ScrollMap.new(smap.BG_Tileset, smap.BG_map, map_width, map_height, tile_width, tile_height)
-smap.FG_smap = ScrollMap.new(smap.BG_Tileset, smap.FG_map, map_width, map_height, tile_width, tile_height)
+smap.BG_smap = ScrollMap.new(smap.BG_Tileset, smap.BG_map, smap.map_width, smap.map_height, smap.tile_width, smap.tile_height)
+smap.FG_smap = ScrollMap.new(smap.BG_Tileset, smap.FG_map, smap.map_width, smap.map_height, smap.tile_width, smap.tile_height)
 smap.BG_blocking_tiles = {129,139,54,55,56,57,63,80,81,82,85,0,1,2,5,16,32,21,37,143,78,94,95,110,126}
 smap.doors = {143}
 smap.stairs = {174}
 smap.default_tile = 17
-
+smap.maxrealx = smap.map_width * smap.tile_width
+smap.maxrealy = smap.map_height * smap.tile_height
 game.level = 2
-hero_startpos = {120 , 120}
-maxrealx = map_width * tile_width
-maxrealy = map_height * tile_height
+smap.hero_startpos = COORD:new({x = 120 , y = 120})
+smap.scroll = COORD:new()
+smap.offset = COORD:new({x=-8,y=-8})
 
 smap.monster_list = {
 					{"Portal",{200 , 200}},
@@ -43,3 +45,7 @@ smap.monster_list = {
 smap.item_list =	{
 					{"key",{150,140}},
 					}
+smap.event_list =   {
+                    {coordm = COORD:new({x=34,y=31}),event_type = "door"},
+                    {coordm = COORD:new({x=42,y=41}),event_type = "stairs", level = "./plans/GAMEOVER.plan.lua"},
+                    }
